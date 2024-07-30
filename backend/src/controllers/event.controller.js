694420f35ce4d73cceac154011d3b6b9df7c71a4
KISHAN JAIN <KISHAN-JAIN>
2024-07-30 22:10:08 +0530

    start event controller

--- backend/src/controllers/event.controller.js
+++ backend/src/controllers/event.controller.js
@@ -3,6 +3,41 @@
  * view all event
  * update event
  * delete event
- * weathe info
+ * weather info
  */
 
+import asyncHandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/apiError.js";
+import ApiResponse from "../utils/ApiResponse.js";
+import Event from "../models/event.model.js"
+
+
+export const createEvent = asyncHandler( async(req, res) => {
+    // event data by client
+    // userId by user
+    // create new event by these data
+    // 
+
+    const {name, date, location} = req.body
+
+    if([name, date, location].some(field => (field?.toString()).trim() === "")){
+        throw new ApiError(400, "Data not received")
+    }
+    
+    const userId = req.userId
+    if(!userId){
+        throw new ApiError(400, "UserId not received")
+    }
+
+    let newEvent
+    try {
+        newEvent = await new Event.create({
+            name, date, location, 
+            createdBy : userId
+        })
+    } catch (error) {
+        throw new ApiError(500, error || "unable to createing Event")
+    }
+
+
+})
